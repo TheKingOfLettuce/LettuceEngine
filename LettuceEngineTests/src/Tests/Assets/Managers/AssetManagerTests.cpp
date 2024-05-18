5ea6f38fb97e510397a42723122351f8980d4d75
TheKingOfLettuce <68159617+TheKingOfLettuce@users.noreply.github.com>
2024-05-18 16:35:47 -0400

    full asset manager tests

--- LettuceEngineTests/src/Tests/Assets/Managers/AssetManagerTests.cpp
+++ LettuceEngineTests/src/Tests/Assets/Managers/AssetManagerTests.cpp
@@ -9,6 +9,8 @@ class CustomAsset : public Asset {
 };
 
 TEST_CASE("AssetManager.AddAsset Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
+
     SECTION("Should add assets") {
         REQUIRE(AssetManager::AddAsset(new Asset("Asset1")));
         REQUIRE(AssetManager::AddAsset(new Asset("Asset2")));
@@ -49,21 +51,220 @@ TEST_CASE("AssetManager.AddAsset Tests", AssetManagerTAG) {
 }
 
 TEST_CASE("AssetManager.GetAsset Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
 
+    SECTION("Should Get Assets") {
+        Asset* asset = new Asset("Asset");
+        CustomAsset* customAsset = new CustomAsset("CustomAsset");
+        AssetManager::AddAsset(asset);
+        AssetManager::AddAsset(customAsset);
+
+        REQUIRE(AssetManager::GetAsset<Asset>("Asset") == asset);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("CustomAsset"));
+    }
+
+    SECTION("Should Not Get Assets If Wrong ID") {
+        AssetManager::AddAsset(new Asset("Asset"));
+        AssetManager::AddAsset(new CustomAsset("CustomAsset"));
+
+        // IDs are case sensitive
+        REQUIRE(AssetManager::GetAsset<Asset>("asset") == nullptr);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("cusomtaSSET") == nullptr);
+        REQUIRE(AssetManager::GetAsset<Asset>("FOOL!") == nullptr);
+    }
+
+    SECTION("Should Not Get Assets If None Added") {
+        REQUIRE(AssetManager::GetAsset<Asset>("nothingtoseehere") == nullptr);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("notAdded") == nullptr);
+    }
+
+    SECTION("Should Add Assets with Same ID but Different Type") {
+        Asset* asset = new Asset("Asset");
+        CustomAsset* customAsset = new CustomAsset("Asset");
+
+        AssetManager::AddAsset(asset);
+        REQUIRE(AssetManager::GetAsset<Asset>("Asset") == asset);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("Asset") == nullptr);
+        AssetManager::AddAsset(customAsset);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("Asset") == customAsset);
+        REQUIRE(AssetManager::GetAsset<Asset>("Asset") != customAsset);
+        REQUIRE(AssetManager::GetAsset<CustomAsset>("Asset") != asset);
+    }
 }
 
 TEST_CASE("AssetManager.RemoveAsset Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
 
+    SECTION("Should Remove Added Assets") {
+        Asset* asset = new Asset("Asset");
+        CustomAsset* customAsset = new CustomAsset("Asset");
+        AssetManager::AddAsset(asset);
+        AssetManager::AddAsset(customAsset);
+
+        REQUIRE(AssetManager::RemoveAsset(asset));
+        REQUIRE(AssetManager::RemoveAsset(customAsset));
+
+        delete asset;
+        delete customAsset;
+    }
+
+    SECTION("Should Remove Added Assets with ID") {
+        Asset* asset = new Asset("Asset");
+        CustomAsset* customAsset = new CustomAsset("Asset");
+        AssetManager::AddAsset(asset);
+        AssetManager::AddAsset(customAsset);
+
+        REQUIRE(AssetManager::RemoveAsset<Asset>(asset->GetAssetID()) == asset);
+        REQUIRE(AssetManager::RemoveAsset<CustomAsset>(customAsset->GetAssetID()) == customAsset);
+
+        delete asset;
+        delete customAsset;
+    }
+
+    SECTION("Should Not Remove Assets if Pointers Not Same") {
+        Asset* asset1 = new Asset("Asset1");
+        Asset* asset1dupe = new Asset("Asset1");
+        AssetManager::AddAsset(asset1);
+        REQUIRE_FALSE(AssetManager::RemoveAsset(asset1dupe));
+
+        delete asset1dupe;
+    }
+
+    SECTION("Should Not Remove If Asset Not Added") {
+        Asset* asset3 = new Asset("Asset3");
+        AssetManager::AddAsset(new Asset("Asset1"));
+        AssetManager::AddAsset(new Asset("Asset2"));
+
+        REQUIRE_FALSE(AssetManager::RemoveAsset(asset3));
+        delete asset3;
+    }
+
+    SECTION("Should Not Remove If ID Not Added") {
+        AssetManager::AddAsset(new Asset("Asset1"));
+        AssetManager::AddAsset(new Asset("Asset2"));
+        AssetManager::AddAsset(new Asset("Asset3"));
+
+        REQUIRE(AssetManager::RemoveAsset<Asset>("Asset42") == nullptr);
+        REQUIRE(AssetManager::RemoveAsset<Asset>("HelloWorld") == nullptr);
+        REQUIRE(AssetManager::RemoveAsset<Asset>("FOOL!") == nullptr);
+    }
+
+    SECTION("Should Throw If Null") {
+        REQUIRE_THROWS_AS(AssetManager::RemoveAsset<Asset>(nullptr), std::invalid_argument);
+    }
 }
 
 TEST_CASE("AssetManager.HasAsset Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
 
+    SECTION("Should Find Asset With ID") {
+        AssetManager::AddAsset(new Asset("Asset1"));
+        AssetManager::AddAsset(new Asset("Asset2"));
+        AssetManager::AddAsset(new Asset("Asset3"));
+
+        REQUIRE(AssetManager::HasAsset<Asset>("Asset1"));
+        REQUIRE(AssetManager::HasAsset<Asset>("Asset2"));
+        REQUIRE(AssetManager::HasAsset<Asset>("Asset3"));
+    }
+
+    SECTION("Should Find Asset With Object") {
+        Asset* asset1 = new Asset("Asset1");
+        Asset* asset2 = new Asset("Asset2");
+        Asset* asset3 = new Asset("Asset3");
+        AssetManager::AddAsset(asset1);
+        AssetManager::AddAsset(asset2);
+        AssetManager::AddAsset(asset3);
+
+        REQUIRE(AssetManager::HasAsset(asset1));
+        REQUIRE(AssetManager::HasAsset(asset2));
+        REQUIRE(AssetManager::HasAsset(asset3));
+    }
+
+    SECTION("Should Not Find Asset Without ID Added") {
+        AssetManager::AddAsset(new Asset("Asset1"));
+        AssetManager::AddAsset(new Asset("Asset2"));
+        AssetManager::AddAsset(new Asset("Asset3"));
+
+        REQUIRE_FALSE(AssetManager::HasAsset<Asset>("Asset42"));
+        REQUIRE_FALSE(AssetManager::HasAsset<Asset>("HelloWorld"));
+        REQUIRE_FALSE(AssetManager::HasAsset<Asset>("FOOL!"));
+    }
+
+    SECTION("Should Not Find Asset If Not Added") {
+        Asset* asset3 = new Asset("Asset3");
+        AssetManager::AddAsset(new Asset("Asset1"));
+        AssetManager::AddAsset(new Asset("Asset2"));
+
+        REQUIRE_FALSE(AssetManager::HasAsset(asset3));
+        delete asset3;
+    }
+
+    SECTION("Should Not Find If Wrong Type") {
+        Asset* asset= new Asset("Asset");
+        AssetManager::AddAsset(asset);
+
+        REQUIRE_FALSE(AssetManager::RemoveAsset<CustomAsset>("Asset"));
+    }
+
+    SECTION("Should Throw If Null") {
+        REQUIRE_THROWS_AS(AssetManager::HasAsset<Asset>(nullptr), std::invalid_argument);
+    }
 }
 
-TEST_CASE("AssetManager.AddAssetCollection Tests", AssetManagerTAG) {
+class TestCollection : public AssetTypeCollection<CustomAsset> {
+        public:
+            TestCollection() : AssetTypeCollection() {}
 
+            bool AddAsset(CustomAsset* asset) override {
+                Flag = true;
+                return AssetTypeCollection::AddAsset(asset);
+            }
+
+            bool Flag = false;
+};
+
+TEST_CASE("AssetManager.AddAssetCollection Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
+
+    SECTION("Should Add Collection") {
+        TestCollection* collection = new TestCollection();
+        REQUIRE(AssetManager::AddAssetCollection(collection));
+    }
+
+    SECTION("Should Not Add Collection If Already Added") {
+        TestCollection* collection = new TestCollection();
+        AssetManager::AddAsset(new CustomAsset("Asset"));
+        REQUIRE_FALSE(AssetManager::AddAssetCollection(collection));
+    }
+
+    SECTION("Should Call Custom Collection") {
+        TestCollection* collection = new TestCollection();
+        AssetManager::AddAssetCollection(collection);
+        AssetManager::AddAsset(new CustomAsset("Asset"));
+        REQUIRE(collection->Flag);
+    }
+
+    SECTION("Should Throw If Null") {
+        REQUIRE_THROWS_AS(AssetManager::AddAssetCollection<Asset>(nullptr), std::invalid_argument);
+    }
 }
 
 TEST_CASE("AssetManager.GetAssetCollection Tests", AssetManagerTAG) {
+    AssetManager::UnloadAllAssets();
 
+    SECTION("Should Get Collection") {
+        TestCollection* collection = new TestCollection();
+        AssetManager::AddAssetCollection(collection);
+        REQUIRE(AssetManager::GetAssetCollection<CustomAsset>() == collection);
+    }
+
+    SECTION("Should Not Get Collection") {
+        TestCollection* collection = new TestCollection();
+        AssetManager::AddAsset(new CustomAsset("Asset"));
+        REQUIRE_FALSE(AssetManager::GetAssetCollection<CustomAsset>() == collection);
+    }
+
+    SECTION("Should Return Null If None Added") {
+        REQUIRE(AssetManager::GetAssetCollection<Asset>() == nullptr);
+    }
 }
--- include/Assets/Managers/AssetManager.h
+++ include/Assets/Managers/AssetManager.h
@@ -11,6 +11,8 @@ class AssetManager {
     public:
         template <typename T>
         static bool AddAsset(T* asset, bool addDefaultCollection = true) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             if (asset == nullptr) {
                 throw std::invalid_argument("Provided asset is null");
             }
@@ -25,6 +27,8 @@ class AssetManager {
         }
         template <typename T>
         static T* GetAsset(std::string id) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             size_t typeID = typeid(T).hash_code();
             if (!HasAssetType(typeID)) {
                 return nullptr;
@@ -35,13 +39,21 @@ class AssetManager {
         }
         template <typename T>
         static bool RemoveAsset(T* asset) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             if (asset == nullptr) {
                 throw std::invalid_argument("Provided asset is null");
             }
-            return RemoveAsset<T>(asset->GetAssetID()) == nullptr;
+            std::string id = asset->GetAssetID();
+            if (!HasAsset<T>(id) || GetAsset<T>(id) != asset) {
+                return false;
+            }
+            return RemoveAsset<T>(id);
         }
         template <typename T>
         static T* RemoveAsset(std::string id) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             size_t typeID = typeid(T).hash_code();
             if (!HasAssetType(typeID)) {
                 return nullptr;
@@ -52,13 +64,17 @@ class AssetManager {
         }
         template <typename T>
         static bool HasAsset(T* asset) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             if (asset == nullptr) {
                 throw std::invalid_argument("Provided asset is null");
             }
-            return HasAsset<T>(asset.GetAssetID());
+            return HasAsset<T>(asset->GetAssetID());
         }
         template <typename T>
         static bool HasAsset(std::string id) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             size_t typeID = typeid(T).hash_code();
             if (!HasAssetType(typeID)) {
                 return false;
@@ -70,6 +86,8 @@ class AssetManager {
 
         template <typename T>
         static bool AddAssetCollection(AssetTypeCollection<T>* collection) {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             if (collection == nullptr) {
                 throw std::invalid_argument("Provided collection is null");
             }
@@ -81,9 +99,11 @@ class AssetManager {
 
         template <typename T>
         static const AssetTypeCollection<T>* GetAssetCollection() {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             size_t typeID = typeid(T).hash_code();
             if (!HasAssetType(typeID)) {
-                throw std::invalid_argument("Provided type has never had assets in the manager");
+                return nullptr;
             }
 
             return static_cast<AssetTypeCollection<T>*>(_assets.at(typeID));
@@ -94,6 +114,8 @@ class AssetManager {
     private:
         template <typename T>
         static bool HasAssetTyoe() {
+            static_assert(std::is_base_of<Asset, T>{},
+            "AssetTypeCollection Type must be of type Asset");
             return HasAssetType(typeid(T).hash_code());
         }
         static bool HasAssetType(size_t typeID);
